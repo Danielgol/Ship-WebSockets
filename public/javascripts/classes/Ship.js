b5791870d3a8556736edf25009f217cd1e79a7d8
Danielgol <danielgol@yahoo.com.br>
2018-01-03 22:39:44 -0300

    adaptando as bolas para naves triangulares

--- public/javascripts/classes/Ship.js
+++ public/javascripts/classes/Ship.js
@@ -4,48 +4,81 @@ function sendShip(ship){
 	socket.emit('ship', ship);
 }
 
-function Ship(){
+function Ship(triangle, x, y){
 
-	this.x = Math.floor(Math.random()*(canvas.width-20+1)+20);
-	this.y = Math.floor(Math.random()*(canvas.height-20+1)+20);
+	this.triangle = triangle;
+	this.x = x;
+	this.y = y;
 	this.angle = 0;
 
 	this.move = function(keys){
 			if (38 in keys) {
-				this.y -= 2;
+				this.triangle.translate(-this.x, -this.y);
+				this.boost();
+				this.triangle.translate(this.x, this.y);
 			}
-			// if(40 in keys) {
-			// 	this.y += 2;
-			// }
 			if (39 in keys) {
-				this.x += 2;
+				this.turn(4);
 			}
 			if (37 in keys) {
-				this.x -= 2;
+				this.turn(-4)
 			}
-			ctx.clearRect(this.x-13, this.y-13, 26, 26);
+			ctx.clearRect(this.x-14, this.y-14, 28, 28);
 			this.obeyLimit(canvas.width, canvas.height);
 			this.drawShip(ctx);
 			sendShip(this);
 	}
 
+	this.turn = function(i){
+			this.angle += i;
+			if(this.angle >= 360){
+	        this.angle = 0;
+	    }else if(this.angle < 0){
+	        this.angle = 359;
+	    }
+			this.triangle.translate(-this.x, -this.y);
+			this.triangle.rotate((Math.PI/180)* i );
+			this.triangle.translate(this.x, this.y);
+	}
+
+	this.boost = function(){
+			if ((this.angle < 90 || this.angle > 270) || (this.angle > 90 && this.angle < 270)) {//CIMA BAIXO
+		        this.y -= Math.cos(this.angle * Math.PI / 180)*2;
+		  }
+		  if ((this.angle < 360 && this.angle > 180) || (this.angle < 180 && this.angle > 0)) { //DIREITA ESQUERDA
+		        this.x += Math.sin(this.angle * Math.PI / 180)*2;
+		  }
+	}
+
 	this.obeyLimit = function(width, height){
 			if(this.x > width){
+					this.triangle.translate(-this.x, -this.y);
 					this.x = 0;
+					this.triangle.translate(this.x, this.y);
 			}else if(this.x < 0){
+					this.triangle.translate(-this.x, -this.y);
 					this.x = width;
+					this.triangle.translate(this.x, this.y);
 			}else if(this.y > height){
+					this.triangle.translate(-this.x, -this.y);
 					this.y = 0;
+					this.triangle.translate(this.x, this.y);
 			}else if(this.y < 0){
+					this.triangle.translate(-this.x, -this.y);
 					this.y = height;
+					this.triangle.translate(this.x, this.y);
 			}
 	}
 
 	this.drawShip = function(ctx){
 			ctx.beginPath();
-			ctx.arc(this.x,this.y,10,0,2*Math.PI);
-			ctx.fillStyle = "white";
-			ctx.fill();
+	    ctx.moveTo(this.triangle['points'][0]['x'], this.triangle['points'][0]['y']);
+	    ctx.lineTo(this.triangle['points'][1]['x'], this.triangle['points'][1]['y']);
+	    ctx.lineTo(this.triangle['points'][2]['x'], this.triangle['points'][2]['y']);
+	    ctx.lineTo(this.triangle['points'][0]['x'], this.triangle['points'][0]['y']);
+			ctx.lineWidth = 1.5;
+			ctx.strokeStyle = "white";
+	    ctx.stroke();
 	    ctx.closePath();
 	}
 
--- public/javascripts/jogo/Execute.js
+++ public/javascripts/jogo/Execute.js
@@ -2,7 +2,7 @@
 var canvas = document.getElementById("mycanvas");
 var ctx = canvas.getContext("2d");
 
-var ship = new Ship();
+var ship;
 
 var keys = [];
 document.addEventListener("keydown", function (e) {
@@ -15,19 +15,44 @@ document.addEventListener("keyup", function (e) {
 socket.on('ship', drawOtherShip);
 
 function drawOtherShip(data){
-	//apagar tudo
+	//apagar rastro
 	ctx.clearRect(data.x-13, data.y-13, 26, 26);
 
 	//desenhar a outra nave
+	// ctx.beginPath();
+	// ctx.arc(data.x,data.y,10,0,2*Math.PI);
+	// ctx.fillStyle = "red";
+	// ctx.fill();
+	// ctx.closePath();
 	ctx.beginPath();
-	ctx.arc(data.x,data.y,10,0,2*Math.PI);
-	ctx.fillStyle = "red";
-	ctx.fill();
+	ctx.moveTo(data.triangle['points'][0]['x'], data.triangle['points'][0]['y']);
+	ctx.lineTo(data.triangle['points'][1]['x'], data.triangle['points'][1]['y']);
+	ctx.lineTo(data.triangle['points'][2]['x'], data.triangle['points'][2]['y']);
+	ctx.lineTo(data.triangle['points'][0]['x'], data.triangle['points'][0]['y']);
+	ctx.lineWidth = 1.5;
+	ctx.strokeStyle = "red";
+	ctx.stroke();
 	ctx.closePath();
+}
+
+function createShip(){
+	var x = Math.floor(Math.random()*(canvas.width-20+1)+20);
+	var y = Math.floor(Math.random()*(canvas.height-20+1)+20);
+	var triangle = new SAT.Polygon(new SAT.Vector(0, 0), [new SAT.Vector(-6, -6),
+  new SAT.Vector(6,-6), new SAT.Vector(0,7)]);
+  triangle.rotate((Math.PI/180)*180);
+  triangle.translate(x, y);
+  var ship = new Ship(triangle, x, y);
+	return ship;
 }
 
 function loop(){
 	ship.move(keys);
 }
 
-setInterval(loop, 10);
+function start(){
+	ship = createShip();
+	setInterval(loop, 10);
+}
+
+start();
