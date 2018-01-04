28250c7504ec7cf30580b0c2c945f28d8bd53aa1
Danielgol <danielgol@yahoo.com.br>
2018-01-03 23:08:44 -0300

    adicionando a inércia

--- public/javascripts/classes/Ship.js
+++ public/javascripts/classes/Ship.js
@@ -9,13 +9,14 @@ function Ship(triangle, x, y){
 	this.triangle = triangle;
 	this.x = x;
 	this.y = y;
+	this.forceX = 0;
+	this.forceY = 0;
 	this.angle = 0;
 
 	this.move = function(keys){
 			if (38 in keys) {
-				this.triangle.translate(-this.x, -this.y);
 				this.boost();
-				this.triangle.translate(this.x, this.y);
+				//this.drawFire(ctx, this.throwFire());
 			}
 			if (39 in keys) {
 				this.turn(4);
@@ -24,6 +25,9 @@ function Ship(triangle, x, y){
 				this.turn(-4)
 			}
 			ctx.clearRect(this.x-14, this.y-14, 28, 28);
+			this.regulateVelocity(2);
+			this.slide(0.001);
+			this.applyForces();
 			this.obeyLimit(canvas.width, canvas.height);
 			this.drawShip(ctx);
 			sendShip(this);
@@ -43,11 +47,60 @@ function Ship(triangle, x, y){
 
 	this.boost = function(){
 			if ((this.angle < 90 || this.angle > 270) || (this.angle > 90 && this.angle < 270)) {//CIMA BAIXO
-		        this.y -= Math.cos(this.angle * Math.PI / 180)*2;
+		           this.forceY += Math.cos(this.angle * Math.PI / 180) * 0.015;
 		  }
 		  if ((this.angle < 360 && this.angle > 180) || (this.angle < 180 && this.angle > 0)) { //DIREITA ESQUERDA
-		        this.x += Math.sin(this.angle * Math.PI / 180)*2;
+		           this.forceX += Math.sin(this.angle * Math.PI / 180) * 0.015;
 		  }
+	}
+
+	this.applyForces = function(){
+		this.triangle.translate(-this.x, -this.y);
+		this.x += this.forceX;
+		this.y -= this.forceY;
+		this.triangle.translate(this.x, this.y);
+	}
+
+	this.slide = function(delay){
+				if (this.forceX > 0) {
+						if (this.forceX - delay > 0) {
+								this.forceX -= delay;
+						} else {
+								this.forceX = 0;
+						}
+				} else if (this.forceX < 0) {
+						if (this.forceX + delay < 0) {
+								this.forceX += delay;
+						} else {
+								this.forceX = 0;
+						}
+				}
+				if (this.forceY > 0) {
+						if (this.forceY - delay > 0) {
+								this.forceY -= delay;
+						} else {
+								this.forceY = 0;
+						}
+				} else if (this.forceY < 0) {
+						if (this.forceY + delay < 0) {
+								this.forceY += delay;
+						} else {
+								this.forceY = 0;
+						}
+				}
+	}
+
+	this.regulateVelocity = function(maxVelocity){
+			if (this.forceX > maxVelocity) {
+					this.forceX = maxVelocity;
+			} else if (this.forceX < (-maxVelocity)) {
+					this.forceX = (-maxVelocity);
+			}
+			if (this.forceY > maxVelocity) {
+					this.forceY = maxVelocity;
+			} else if (this.forceY < (-maxVelocity)) {
+					this.forceY = (-maxVelocity);
+			}
 	}
 
 	this.obeyLimit = function(width, height){
@@ -69,6 +122,29 @@ function Ship(triangle, x, y){
 					this.triangle.translate(this.x, this.y);
 			}
 	}
+
+	// this.throwFire = function(){
+	// 		var losangle = new SAT.Polygon(new SAT.Vector(0, 0),
+	// 		[new SAT.Vector(-3, -3), new SAT.Vector(0,-5), new SAT.Vector(3,-3), new SAT.Vector(0,5)]);
+	// 		losangle.translate(this.x, this.y);
+	// 		losangle.translate(-this.x, -(this.y-13));
+	// 		losangle.rotate((Math.PI/180)* this.angle );
+	// 		losangle.translate(this.x, this.y);
+	// 		return losangle;
+	// }
+
+	// this.drawFire = function(ctx, losangle){
+	// 		ctx.beginPath();
+	// 		ctx.lineWidth = 1;
+	// 		ctx.moveTo(losangle['points'][0]['x'], losangle['points'][0]['y']);
+	// 		ctx.lineTo(losangle['points'][1]['x'], losangle['points'][1]['y']);
+	// 		ctx.lineTo(losangle['points'][2]['x'], losangle['points'][2]['y']);
+	// 		ctx.lineTo(losangle['points'][3]['x'], losangle['points'][3]['y']);
+	// 		ctx.lineTo(losangle['points'][0]['x'], losangle['points'][0]['y']);
+	// 		ctx.strokeStyle = "white";
+	// 		ctx.stroke();
+	// 		ctx.closePath();
+	// }
 
 	this.drawShip = function(ctx){
 			ctx.beginPath();
